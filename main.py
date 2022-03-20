f057f463ee41499befac35b2ca3da82f97fedc66
rmbailey86 <82070517+rmbailey86@users.noreply.github.com>
2022-03-19 20:34:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,9 @@
 # Tutor by Thomas Dewey
 
 import mysql.conncetor
+
+def connect():
+     # connect address : port
+     server_ip = ##the ip address of  your database server##
+     con = mysql.connector.connect(host=server_ip, user='maxuser', password='maxpwd', port='4000')
+     return con
