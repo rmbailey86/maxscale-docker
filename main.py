1d5d5960ac8e354d4ba81d43d781ebbf9557d3c3
rmbailey86 <82070517+rmbailey86@users.noreply.github.com>
2022-03-20 15:34:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,54 +3,60 @@
 ## Robert Bailey, rmbailey@student.rtc.edu
 ## Tutor by Thomas Dewey
 
-import mysql.conncetor
+import mysql.connector
+
 
 def connect():
-     # connect address : port
-     server_ip = "10.0.0.224"
-     con = mysql.connector.connect(host=server_ip, user='maxuser', password='maxpwd', port='4000')
-     return con
+    # connect address : port
+    server_ip = "10.0.0.224"
+    con = mysql.connector.connect(host=server_ip, user='maxuser', password='maxpwd', port='4000')
+    return con
+
 def question_one(cursor):
-     query = "SELECT Zipcode FROM zipcodes_one.zipcodes_one ORDER BY Zipcode DESC LIMIT 10;"
-     cursor.execute(query)
-     data = cursor.fetchall()
-     print ('Last 10 of zipcodes 1')
-     for row in data:
-          print (row)
-     return   
+    query = "SELECT Zipcode FROM zipcodes_one.zipcodes_one ORDER BY Zipcode DESC LIMIT 10;"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    print('Last 10 of zipcodes 1')
+    for row in data:
+        print(row)
+    return
+
 def question_two(cursor):
-     query = "SELECT Zipcode FROM zipcodes_two.zipcodes_two ORDER BY Zipcode LIMIT 10;"
-     cursor.execute(query)
-     data = cursor.fetchall()
-     print ('first 10 of zipcodes 2')
-     for row in data:
-          print (row)
-     return   
+    query = "SELECT Zipcode FROM zipcodes_two.zipcodes_two ORDER BY Zipcode LIMIT 10;"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    print('first 10 of zipcodes 2')
+    for row in data:
+        print(row)
+    return
+
 def question_three(cursor):
     query = "SELECT MAX(DISTINCT Zipcode) FROM zipcodes_one.zipcodes_one;"
     cursor.execute(query)
     data = cursor.fetchall()
-    print ('Largest in Zipcodes 1')
-    print (data)
+    print('Largest in Zipcodes 1')
+    print(data)
     return
+
 def question_four(cursor):
     query = "SELECT MIN(DISTINCT Zipcode) FROM zipcodes_two.zipcodes_two;"
     cursor.execute(query)
     data = cursor.fetchall()
-    print ('Smallest in Zipcodes 2')
-    print (data)
+    print('Smallest in Zipcodes 2')
+    print(data)
     return
+
 def main():
-     conn = connect()
-     curs = conn.cursor()
-     question_one(curs)
-     question_two(curs)
-     question_three(curs)
-     question_four(curs)
-     conn.close()
-     
- if __name__ == '__main__':
-     main()
+    conn = connect()
+    curs = conn.cursor()
+    question_one(curs)
+    question_two(curs)
+    question_three(curs)
+    question_four(curs)
+    conn.close()
+
+if __name__ == '__main__':
+    main()
      
      
      
